aaffc46c38805da6a5f2ddb77d93c837359756cf
KLan Stovall <kland13@gmail.com>
2021-11-15 11:10:40 -0800

    initial backend setup

---
+++ server.js
@@ -0,0 +1,22 @@
+'use strict';
+
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 3001;
+
+mongoose.connect(process.env.DB_URL, {useNewUrlParser: true, useUnifiedTopology: true});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('Mongoose is connected')
+});
+
+app.listen(PORT, () => console.log(`listening on ${PORT}`));
