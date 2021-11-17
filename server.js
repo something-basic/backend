92eeec3c6e7091f7953bf8fca2253be6a1be7ce0
KLan Stovall <kland13@gmail.com>
2021-11-16 18:27:32 -0800

    day objects works

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.listen(PORT, () => console.log(`listening on ${PORT}`));
 // Back end calculates the query string array's after and before dates accordingly
 async function getHistoricalCounts(req, res) {
   
-  const verified = verifyToken(req);
+  const verified =  await verifyToken(req);
   // Make array of dates
   try {
     
@@ -40,11 +40,11 @@ async function getHistoricalCounts(req, res) {
       const millisecondsPerDay = 1000*60*60*24;
       const millisecondsArray = [];
       for (let i = 0; i < 7; i++) {
-        millisecondsArray.push(dateNow - millisecondsPerDay);
+        millisecondsArray.push(dateNow - millisecondsPerDay * i);
       }
       
       // Google API expects seconds from UNIX epoch. Convert from milliseconds to seconds.
-      const secondsArray = millisecondsArray.map( date => date/1000 );
+      const secondsArray = millisecondsArray.map( date => Math.round(date/1000) );
     
       // Get resultsSizeEstimate for each date in array of dates. Range is
       // const countsArray = [];
@@ -54,10 +54,10 @@ async function getHistoricalCounts(req, res) {
       //   getCountInDateRange(user, startDate, endDate);
       // }
       
-      const values = await Promise.all([getCountInDateRange(user, secondsArray[6], secondsArray[5]), getCountInDateRange(user, secondsArray[5], secondsArray[4])]);
-    
+      const values = await Promise.all([getCountInDateRange(verified, secondsArray[6], secondsArray[5], req), getCountInDateRange(verified, secondsArray[5], secondsArray[4], req)]);
+      // const values = await getCountInDateRange(verified, secondsArray[1], secondsArray[0], req);
       // Return: [{date: value, total: value, unread: value},{},{},...,{}]
-      return values;
+      res.status(200).send(values);
     } else {
       res.status(404).send('IT NOT WORK  ¯\_(ツ)_/¯ ');
     }
@@ -69,19 +69,24 @@ async function getHistoricalCounts(req, res) {
 
 }
 
-async function getCountInDateRange(user, startDate, endDate) {
+async function getCountInDateRange(user, startDate, endDate, req) {
+
+  const total = await queryAPI(user, `after:${startDate} before:${endDate}`, req);
+  const unread = await queryAPI(user, `after:${startDate} before:${endDate} is:unread`, req);
+  
   const dayObj = {
     date: startDate,
-    total: await queryAPI(user, `after:${startDate} before:${endDate}`),
-    unread: await queryAPI(user, `after:${startDate} before:${endDate} is:unread`)
+    total: total,
+    unread: unread
   }
+  console.log(dayObj);
   return dayObj;
 }
 
-async function queryAPI(user, q) {
+async function queryAPI(user, q, req) {
   const url = `https://gmail.googleapis.com/gmail/v1/users/${user}/messages?maxResults=1000&q="${q}"`;
   const apiResponse = await axios.get(url, { headers: {"Authorization": req.headers.authorization} });
-  return apiResponse.data.resultsSizeEstimate;
+  return apiResponse.data.resultSizeEstimate;
 }
 
 async function getCounts(req, res) {
