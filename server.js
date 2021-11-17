0f8eadd8db1e2e0b71b2c9f9f46977e0747104a5
Chloe Nott <chloe@nott.email>
2021-11-16 20:43:06 -0800

    Added binOption query support

--- server.js
+++ server.js
@@ -19,69 +19,84 @@ app.get('/history', getHistoricalCounts);
 mongoose.connect(process.env.DB_URL, {useNewUrlParser: true, useUnifiedTopology: true});
 
 const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
+db.on('error', console.error.bind(console, 'Connection error:'));
 db.once('open', function() {
   console.log('Mongoose is connected')
 });
 
-app.listen(PORT, () => console.log(`listening on ${PORT}`));
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
  
 
-// Front end sends to back end one of these strings: 'Last 7 days','Last 30 days', 'Last 12 months', 'All time'
-// Back end calculates the query string array's after and before dates accordingly
-async function getHistoricalCounts(req, res) {
-  
-  const verified =  await verifyToken(req);
 
+// Returns an array of objects containing the bin start date, total email count, and unread email count: [{date: value, total: value, unread: value}]
+// Request query should contain one of these strings: 'Last 7 days','Last 30 days', 'Last 12 months', 'All time'. Eg: "/history?Last 7 days"
+async function getHistoricalCounts(req, res) {
   try {
+    const verified =  await verifyToken(req);
 
     if(verified) {
-      const dateNow = Date.now()/1000; // Gmail API uses seconds elapsed since the UNIX epoch.
-      const secondsPerDay = 60*60*24;
-      const secondsArray = [];
-      for (let i = 0; i <= 7; i++) {
-        secondsArray.push(Math.round(dateNow - secondsPerDay * i));
+      const dateNow = Date.now()/1000; // Seconds elapsed since UNIX epoch to match Gmail API expectation.
+
+      // Frontend must select one of these 4 binOption strings via query when getting history data. Eg: "/history?Last 7 days"
+      const binOptions = [{
+        binOption: 'Last 7 days',
+        binSize: 60*60*24, // 1 day represented in seconds.
+        binCount: 7 // 7 days
+      },{
+        binOption: 'Last 30 days',
+        binSize: 60*60*24, // 1 day represented in seconds.
+        binCount: 30 // 30 days
+      },{
+        binOption: 'Last 12 months',
+        binSize: 60*60*24 * 30, // 1 month represented in seconds.
+        binCount: 12 // 12 months
+      },{
+        binOption: 'Last 5 years',
+        binSize: 60*60*24 * 30, // 1 month represented in seconds.
+        binCount: 60 // 60 months
+      }]
+
+      // Create date array that represents the bins
+      let datesArray = [];
+      const selectedBinOption = binOptions.filter(option => option.binOption === req.query.binOption)[0];
+      for (let i = 0; i <= selectedBinOption.binCount; i++) {
+        datesArray.push(Math.round(dateNow - selectedBinOption.binSize * i));
       }
-    
-      // Get resultsSizeEstimate for each date in array of dates. Range is
+
+      // Get email total and unread counts for each bin.
       let binResults = [];
-      for (let i = 0; i < secondsArray.length - 1; i++) {
-        const beforeDate = secondsArray[i];
-        const afterDate = secondsArray[i+1];
-        //console.log(beforeDate, afterDate);
-        binResults.push(getCountInDateRange(verified, beforeDate, afterDate, req));
+      for (let i = 0; i < datesArray.length - 1; i++) {
+        const beforeDate = datesArray[i];
+        const afterDate = datesArray[i+1];
+        binResults.push(getBinData(verified, beforeDate, afterDate, req));  // Functions in binResults get gmail API data simultaneously.
       }
       
       const values = await Promise.all(binResults);
-      console.log(values)
+      res.status(200).send(values);   // Returns array of objects: [{date: value, total: value, unread: value}]
 
-      res.status(200).send(values);   // Returns array of objects: [{date: value, total: value, unread: value},{},{},...,{}]
     } else {
-      res.status(404).send('IT NOT WORK  ¯\_(ツ)_/¯ ');
+      res.status(498).send('Token expired/invalid');
     }
 
   } catch (e) {
     console.error(e)
-    res.status(500).send('HISTORY NOT FOUND')
+    res.status(500).send('Internal server error')
   }
 
 }
 
-async function getCountInDateRange(user, beforeDate, afterDate, req) {
-  console.log(user, beforeDate, afterDate);
-  const total = await queryAPI(user, `after:${afterDate} before:${beforeDate}`, req);
-  const unread = await queryAPI(user, `after:${afterDate} before:${beforeDate} is:unread`, req);
-  
-  const dayObj = {
-    date: beforeDate*1000,  // ChartJS uses timestamps in milliseconds since UNIX epoch
-    total: total,
-    unread: unread
+async function getBinData(user, beforeDate, afterDate, req) {
+  const binResults = {
+    date: beforeDate*1000,  // Date is represented as milliseconds since UNIX epoch to match ChartJS timestamps.
+    total: await getGMailData(user, `after:${afterDate} before:${beforeDate}`, req),
+    unread: await getGMailData(user, `after:${afterDate} before:${beforeDate} is:unread`, req)
   }
-  return dayObj;
+
+  return binResults;
 }
 
-async function queryAPI(user, q, req) {
-  const url = `https://gmail.googleapis.com/gmail/v1/users/${user}/messages?maxResults=1000&q="${q}"`;
+async function getGMailData(user, q, req) {
+  const url = `https://gmail.googleapis.com/gmail/v1/users/${user}/messages?maxResults=25000&q="${q}"`;
   const apiResponse = await axios.get(url, { headers: {"Authorization": req.headers.authorization} });
   return apiResponse.data.resultSizeEstimate;
 }
@@ -90,14 +105,11 @@ async function getCounts(req, res) {
   try {
     const verified = await verifyToken(req)
     if (verified) {
-      console.log(verified);
       const url = `https://gmail.googleapis.com/gmail/v1/users/${verified}/messages?maxResults=1000&q=""`;
-      console.log('hi2')
       const data = await axios.get(url, { headers: {Authorization: req.headers.authorization} });
-      console.log(`${data.data.resultSizeEstimate}`)
       res.status(200).send(`${data.data.resultSizeEstimate}`)
     } else {
-      res.status(404).send('IT NOT WORK  ¯\_(ツ)_/¯ ')
+      res.status(498).send('Token expired/invalid')
     }
     
   } catch (e) {
